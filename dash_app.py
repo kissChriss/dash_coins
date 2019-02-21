a0aebf91baf0fb4a69874ef625203f4c8da3abae
kissChriss <32354154+kissChriss@users.noreply.github.com>
2019-02-21 23:40:30 +0300

    with tabs

--- dash_app.py
+++ dash_app.py
@@ -21,12 +21,26 @@ def complex_query_func(q_num, c_name, c_value):
 	answer = cur.fetchall()
 	return answer
 
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+app.config['suppress_callback_exceptions']=True
 app.layout = html.Div([
-	html.H3('Search'),
+
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Search', value='tab-1'),
+        dcc.Tab(label='Add new', value='tab-2'),
+        dcc.Tab(label='Change status', value='tab-3'),
+    ]),
+    html.Div(id='tabs-content')
+
+	])
+
+
+search_layout = html.Div([
+		html.H3('Search coin in the database'),
 
 	dcc.Dropdown(
 		id ='country_dropdown',
@@ -40,18 +54,26 @@ app.layout = html.Div([
 	options=[{'label': i[0], 'value': i[0]} for i in query_func(1)],
 	value='2 euro'),
 
-	html.Div(id='first_output'),
-
-	html.Br(), 
-
-	html.H3('Add new'),
-
-	html.Br(), 
-
-	html.H3('Change status')
-
+	html.Div(id='first_output')
 	])
 
+add_layout = html.Div([
+	html.H3('Add new coin to the database')
+	])
+
+change_layout = html.Div([
+	html.H3('Change coin\'s status')])
+
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return search_layout
+    elif tab == 'tab-2':
+        return add_layout
+    elif tab == 'tab-3':
+    	return change_layout    
 
 
 @app.callback(
